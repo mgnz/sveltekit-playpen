0975d76f9e0677dd4d015fcdbcfb5ccbcd6b2d16
Steven Higgan <stevenh@mad.geek.nz>
2023-01-14 02:40:28 +1300

    default layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+</script>
+
+<h1>from /+layout.svelte</h1>
+
+<slot />
+
+<style lang="scss">
+	@import '../styles/reset.scss';
+	@import '../styles/base.scss';
+	@import '../styles/app.scss';
+</style>
