7e9f20c78aa4332c52b6b48f04e2b892ab1fc21d
Steven Higgan <stevenh@mad.geek.nz>
2023-01-14 02:39:30 +1300

    adding sass

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,14 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import sveltePreprocess from 'svelte-preprocess'
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
+export default {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+
+	preprocess: [vitePreprocess(), sveltePreprocess()],
 
 	kit: {
 		adapter: adapter()
 	}
 };
-
-export default config;
