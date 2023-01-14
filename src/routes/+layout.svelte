4c933c72a786b3e92ba55fc19c7e0fee92bb3ce6
Steven Higgan <stevenh@mad.geek.nz>
2023-01-15 01:18:12 +1300

    code tidy for base layout and page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,18 @@
 <script lang="ts">
+	import '$lib/styles/app.scss';
+
+	const meta_description = `Awesome examples of SvelteKit sites in the wild`;
+
+	const now = new Date();
 </script>
 
-<h1>from /+layout.svelte</h1>
+<svelte:head>
+	<title>Awesome SvelteKit</title>
+	<meta name="description" content={meta_description} />
+	<meta name="og:description" content={meta_description} />
+	<meta name="twitter:creator" content="@jrib_" />
+</svelte:head>
+
+<h1>from /+layout.svelte {now} dd</h1>
 
 <slot />
-
-<style lang="scss">
-	@import '../styles/reset.scss';
-	@import '../styles/base.scss';
-	@import '../styles/app.scss';
-</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import TypiCodePhoto from '$lib/components/TypiCodePhoto.svelte';
+	import Counter from '$lib/components/Counter.svelte';
+</script>
+
+<body>
+	<h1>Welcome to SvelteKit</h1>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation ....</p>
+	<Counter />
+	<!-- <TypiCodePhoto /> -->
+</body>
+
+<style lang="scss">
+	a {
+		color: #dd1472;
+		text-decoration: none;
+
+		&:hover {
+			color: #55ed37;
+			text-decoration: none;
+		}
+	}
+</style>
