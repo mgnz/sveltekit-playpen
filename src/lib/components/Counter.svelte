0e4f06b8ba3969df40a53c3fcd283461d218800b
Steven Higgan <stevenh@mad.geek.nz>
2023-01-14 16:58:49 +1300

    adding some play components

---
+++ src/lib/components/Counter.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	let count: number = 0;
+	const increment = () => {
+		count += 1;
+	};
+</script>
+
+<button on:click="{increment}">
+	<h1>count is {count}</h1>
+</button>
+
+<style lang="scss">
+	button {
+		h1 {
+			color: red;
+		}
+	}
+</style>
---
+++ src/lib/components/TypiCodePhoto.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	let post: any = {};
+
+	const getPost = async () => {
+		const response = await fetch('https://jsonplaceholder.typicode.com/photos/1');
+		const json = await response.json();
+
+		console.log(json);
+		return json;
+	};
+
+	$: getPost().then((val) => {
+		post = val;
+	});
+</script>
+
+<h1>{post.title}</h1>
+<img src={post.thumbnailUrl} />
+
+<style lang="scss">
+</style>
